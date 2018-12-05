2e7b7e1f35314c193c3ce1686a994a3188f070bd
chals <fison67@nate.com>
2018-12-05 12:43:37 +0900

    Create ha-temperature-sensor.groovy

---
+++ devicetypes/fison67/ha-temperature-sensor.src/ha-temperature-sensor.groovy
@@ -0,0 +1,143 @@
+/**
+ *  HA Temperature Sensor (v.0.0.1)
+ *
+ *  Authors
+ *   - fison67@nate.com
+ *  Copyright 2018
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+ 
+import groovy.json.JsonSlurper
+
+metadata {
+	definition (name: "HA Temperature Sensor", namespace: "fison67", author: "fison67") {
+        capability "Temperature Measurement"
+      	capability "Sensor"
+        capability "Refresh"		
+        
+        attribute "lastCheckin", "Date"
+         
+        command "setStatus"
+	}
+
+
+	simulator {
+	}
+    
+    preferences {
+	}
+
+	tiles {
+		multiAttributeTile(name:"temperature", type:"generic", width:6, height:4) {
+            tileAttribute("device.temperature", key: "PRIMARY_CONTROL") {
+                attributeState("temperature", label:'${currentValue}°',
+                    backgroundColors:[
+                        // Fahrenheit color set
+                        [value: 0, color: "#153591"],
+                        [value: 5, color: "#1e9cbb"],
+                        [value: 10, color: "#90d2a7"],
+                        [value: 15, color: "#44b621"],
+                        [value: 20, color: "#f1d801"],
+                        [value: 25, color: "#d04e00"],
+                        [value: 30, color: "#bc2323"],
+                        [value: 44, color: "#1e9cbb"],
+                        [value: 59, color: "#90d2a7"],
+                        [value: 74, color: "#44b621"],
+                        [value: 84, color: "#f1d801"],
+                        [value: 95, color: "#d04e00"],
+                        [value: 96, color: "#bc2323"]
+					]
+                )
+            }
+            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'Updated: ${currentValue}\n')
+            }
+        }   
+        
+        valueTile("tmp_label", "", decoration: "flat", width: 4, height: 2) {
+            state "default", label:""
+        }
+        
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label:"", action:"refresh", icon:"st.secondary.refresh"
+        }
+        
+        valueTile("ha_url", "device.ha_url", width: 3, height: 1) {
+            state "val", label:'${currentValue}', defaultState: true
+        }
+        
+        valueTile("entity_id", "device.entity_id", width: 3, height: 1) {
+            state "val", label:'${currentValue}', defaultState: true
+        }
+        
+	}
+
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+}
+
+def setStatus(String value){
+    if(state.entity_id == null){
+    	return
+    }
+	log.debug "Status[${state.entity_id}] >> ${value}"
+    
+    def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
+    	
+    sendEvent(name: "temperature", value: value as double)
+    sendEvent(name: "lastCheckin", value: now, displayed: false)
+    sendEvent(name: "entity_id", value: state.entity_id, displayed: false)
+}
+
+def setHASetting(url, password, deviceId){
+	state.app_url = url
+    state.app_pwd = password
+    state.entity_id = deviceId
+    
+    sendEvent(name: "ha_url", value: state.app_url, displayed: false)
+}
+
+def refresh(){
+	log.debug "Refresh"
+    def options = [
+     	"method": "GET",
+        "path": "/api/states/${state.entity_id}",
+        "headers": [
+        	"HOST": state.app_url,
+            "x-ha-access": state.app_pwd,
+            "Content-Type": "application/json"
+        ]
+    ]
+    sendCommand(options, callback)
+}
+
+def callback(physicalgraph.device.HubResponse hubResponse){
+	def msg
+    try {
+        msg = parseLanMessage(hubResponse.description)
+		def jsonObj = new JsonSlurper().parseText(msg.body)
+        setStatus(jsonObj.state)
+    } catch (e) {
+        log.error "Exception caught while parsing data: "+e;
+    }
+}
+
+def updated() {
+}
+
+def sendCommand(options, _callback){
+	def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: _callback])
+    sendHubCommand(myhubAction)
+}
