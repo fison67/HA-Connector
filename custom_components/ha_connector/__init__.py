29e1308b5e561ca9bd5c8a8856656f557f8be731
miumida <46196137+miumida@users.noreply.github.com>
2020-11-26 20:52:57 +0900

    Update __init__.py

--- custom_components/ha_connector/__init__.py
+++ custom_components/ha_connector/__init__.py
@@ -13,16 +13,53 @@ import logging
 import json
 import base64
 
+import voluptuous as vol
+
 import homeassistant.loader as loader
 from homeassistant.const import (STATE_UNKNOWN, EVENT_STATE_CHANGED)
+import homeassistant.helpers.config_validation as cv
+from homeassistant import config_entries
 #from homeassistant.remote import JSONEncoder
 
 DOMAIN = "ha_connector"
 
+CONF_APP_URL = 'app_url'
+CONF_APP_ID  = 'app_id'
+CONF_ACCESS_TOKEN = 'access_token'
+
+
+def base_config_schema(config: dict = {}) -> dict:
+    """Return a shcema configuration dict for HA-Connector."""
+    if not config:
+        config = {
+            CONF_APP_URL: "",
+            CONF_APP_ID: "",
+            CONF_ACCESS_TOKEN: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        }
+    return {
+        vol.Required(CONF_APP_URL): str,
+        vol.Required(CONF_APP_ID): str,
+        vol.Required(CONF_ACCESS_TOKEN, default=config.get(CONF_ACCESS_TOKEN)): str,
+    }
+
+
+def config_combined() -> dict:
+    """Combine the configuration options."""
+    base = base_config_schema()
+
+    return base
+
+CONFIG_SCHEMA = vol.Schema({DOMAIN: config_combined()}, extra=vol.ALLOW_EXTRA)
+
 def setup(hass, config):
-    app_url = config[DOMAIN].get('app_url')
-    app_id = config[DOMAIN].get('app_id')
-    access_token = config[DOMAIN].get('access_token')
+
+    if DOMAIN not in config:
+        return True
+
+    app_url = config[DOMAIN][CONF_APP_URL] #.get('app_url')
+    app_id  = config[DOMAIN][CONF_APP_ID] #.get('app_id')
+    access_token = config[DOMAIN][CONF_ACCESS_TOKEN] #.get('access_token')
+
     registerList = getRegisteredHADeviceList(app_url, app_id, access_token)
 
     def event_listener(event):
@@ -56,7 +93,7 @@ def setup(hass, config):
     def do_refresh(call):
         nonlocal registerList
         registerList = getRegisteredHADeviceList(app_url, app_id, access_token)
-            
+
 
     hass.bus.listen(EVENT_STATE_CHANGED, event_listener)
     hass.services.register(DOMAIN, "refresh", do_refresh)
@@ -67,3 +104,15 @@ def setup(hass, config):
 def getRegisteredHADeviceList(app_url, app_id, access_token):
     response = requests.get(app_url + app_id + "/getHADevices?access_token=" + access_token)
     return json.loads(response.text)['list']
+
+
+async def async_setup_entry(hass, config_entry):
+    """Set up this integration using UI."""
+
+    if hass.data.get(DOMAIN) is not None:
+        return False
+    if config_entry.source == config_entries.SOURCE_IMPORT:
+        hass.async_create_task(hass.config_entries.async_remove(config_entry.entry_id))
+        return False
+
+    return True
